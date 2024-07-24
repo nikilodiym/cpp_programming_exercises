65e818d6c9f5814cec8c96695dda667dbf02b1fb
Nikita Krenov <nikitabaram@gmail.com>
2024-07-24 12:27:27 +0300

    Point

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 
 using namespace std;
 
+struct Point {
+	double x, y;
+};
+
+double distance(const Point& p1, const Point& p2) {
+	return sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+}
+
 int main() {
+	Point p1 = { 0.0, 0.0 };
+	Point p2 = { 3.0, 4.0 };
+
+	cout << "Distance between points: " << distance(p1, p2) << endl;
 
 	system("pause");
 	return 0;
